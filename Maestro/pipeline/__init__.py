d49ac3c1e46d86a79ae96a03cbdfa7d265d8b499
herrahmo <78833338+herrahmo@users.noreply.github.com>
2021-10-25 14:31:03 -0700

    manual merging vision attack and defense test 1

--- Maestro/pipeline/__init__.py
+++ Maestro/pipeline/__init__.py
@@ -2,5 +2,5 @@ from Maestro.pipeline.pipeline import AugmentedPipelineCV, LossFuncPipelineCV, S
 from Maestro.pipeline.AutoPipeline import AutoPipelineAugmentedCV, AutoPipelineLossFuncCV
 
 
-from Maestro.pipeline.pipeline import Pipeline, VisionPipeline, Scenario, AttackerAccess
+from Maestro.pipeline.pipeline import VisionPipeline, Scenario, AttackerAccess
 from Maestro.pipeline.AutoPipeline import AutoPipelineForVision

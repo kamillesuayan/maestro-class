2c902715e7dd59a42637dc97002b3496150351a0
junlin <junlinwang18@gmail.com>
2021-10-25 14:42:35 -0700

    removed Pipeline class

--- Maestro/pipeline/__init__.py
+++ Maestro/pipeline/__init__.py
@@ -1,6 +1,15 @@
-from Maestro.pipeline.pipeline import AugmentedPipelineCV, LossFuncPipelineCV, Scenario, DefenseAccess
-from Maestro.pipeline.AutoPipeline import AutoPipelineAugmentedCV, AutoPipelineLossFuncCV
-
-
-from Maestro.pipeline.pipeline import Pipeline, VisionPipeline, Scenario, AttackerAccess
-from Maestro.pipeline.AutoPipeline import AutoPipelineForVision
+from Maestro.pipeline.pipeline import (
+    AugmentedPipelineCV,
+    LossFuncPipelineCV,
+    Scenario,
+    DefenseAccess,
+    # Pipeline,
+    VisionPipeline,
+    Scenario,
+    AttackerAccess,
+)
+from Maestro.pipeline.AutoPipeline import (
+    AutoPipelineAugmentedCV,
+    AutoPipelineLossFuncCV,
+    AutoPipelineForVision,
+)

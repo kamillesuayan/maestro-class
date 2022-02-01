eadca56b6b56bdc6c789d8647e204698b82ff650
kamillesuayan <kamille.suayan@gmail.com>
2022-02-01 12:58:35 -0800

    Update attack_project-AdversarialGworl.py

--- playground/attack_project/attack_project-AdversarialGworl.py
+++ playground/attack_project/attack_project-AdversarialGworl.py
@@ -13,7 +13,7 @@ class ProjectAttack:
         vm,
         image_size: List[int],
         l2_threshold=7.5,
-        steps=6, # strong correlation with distance??
+        steps=7, # strong correlation with distance??
         alpha=0.2 # tune
         
     ):
@@ -29,7 +29,8 @@ class ProjectAttack:
         original_image:  np.ndarray,
         labels: List[int],
         target_label: int,
-        epsilon = 0.01 # tune
+        #epsilon = 0.01 # tune
+        epsilon = 0.021
 
     ):
         """
